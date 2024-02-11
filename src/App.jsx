3d9e18ccc883ad414b63bfb4f09366dde0ca5666
Ragini Kumari <ragoinichaudhary282@gmail.com>
2024-02-11 10:11:12 +0530

    file added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Home from './assets/Home.jsx'
+import MyComponent from './assets/MyComponent.jsx'
+
+function App(){
+  return(
+    <>
+    <h1>React js</h1>
+    <Home/><br></br>
+    <MyComponent/>
+    </>
+
+  )
+}
+export default App
